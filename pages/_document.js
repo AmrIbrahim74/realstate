dcb2b9af76da34e1f6501a56047cadd192eb7b2d
AmrIbrahim74 <amr.brahim74@gmail.com>
2023-03-14 01:30:02 +0200

    meta seo

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="description" content="Realstate in UAE" />
+      <meta name="keywords" content="Realstate, Buy, Rent, Dubai, UAE" />
+      <meta name="author" content="John Doe" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       <body>
         <Main />
         <NextScript />
